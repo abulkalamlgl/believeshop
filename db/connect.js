b1b7f864bb9fd45b478513f2b8e925409389d4ca
Md Mijanur Rahaman <92916041+abulkalamlgl@users.noreply.github.com>
2021-11-01 21:05:00 +0530

    changes date 01.11.2021

--- db/connect.js
+++ db/connect.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose')
 
 
 
-mongoose.connect('mongodb://localhost:27017/userData',{
+mongoose.connect('mongodb+srv://admin-abulkalam:abulkalam123@clustertest.gvmyx.mongodb.net/believeShopDatabase?retryWrites=true&w=majority',{
     useNewUrlParser:true,
     useUnifiedTopology:true
 }).then(()=>{
